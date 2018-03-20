b0e49e3f8c6d69f3909bc7e5e8e1303c9ed08da8
ARTHUR-PC\Arthur <arthur.rozo@outlook.fr>
2018-03-20 11:53:52 +0100

    10 garantie

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const db = mongoose.connect('mongodb://localhost:32768/stock')
 const app = express()
+const YahooFinanceAPI = require('yahoo-finance-data');
+var currencies = []
+const api = new YahooFinanceAPI({
+    key: 'dj0yJmk9SVZLdFJHeEF0MU55JmQ9WVdrOVRUSmhlWHB0TjJVbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yMA--',
+    secret: 'a917dc088aed919c3e9613bc83b341d34f0898ed'
+})
 app.use(bodyParser.json())
 var Schema = mongoose.Schema
 var stockSchema = new Schema({
@@ -19,12 +25,21 @@ app.use(function (req, res, next) {
 app.use(bodyParser.json());
 app.get('/currencies', function (req, res, next) {
     res.statusCode = 200
-    return res.json([
-        { name: 'Google',symbol:"GOO", price: '1' },
-        { name: 'Facebook', price: '3' },
-        { name: 'Amazon', price: '4' },
-        { name: 'Apple', price: '5' },
-    ])
+    api.getRealtimeQuotes('CGGDS.PA,ATA.PA,ACALS.PA,ORA.PA,JXR.PA,FP.PA,CA.PA,SAN.PA,YHOO,MSFT,AAPL').then(
+        function (data) {
+            currencies=[]
+            var tmp = data.query.results.quote
+            tmp.forEach(element => {
+                var one = new Stock();
+                one.name = element.Name
+                one.symbol = element.Symbol
+                one.price = element.LastTradePriceOnly
+                currencies.push(one)
+            })
+
+        }
+    )
+    res.json(currencies)
 })
 app.get('/cart', function (req, res, next) {
     res.statusCode = 200
@@ -49,16 +64,33 @@ app.post('/cart', function (req, res, next) {
         }
     })
 })
-app.post('/cartSell',function(req,res,next){
+app.post('/cartSell', function (req, res, next) {
     var stock = new Stock(req.body)
-    stock.remove({name:stock.name},function(err){
-        if(!err){
-            res.statusCode=200
+    stock.remove({ name: stock.name }, function (err) {
+        if (!err) {
+            res.statusCode = 200
         }
-        else{
+        else {
             console.log(err)
         }
     })
+})
+app.post('/infos', function (req, res, next){
+    var infos=[]
+    var name = req.body.name
+    var priceA=req.body.price
+    var priceN=null
+    var theOne=new Object()
+    api.getRealtimeQuotes(req.body.symbol).then(function(data){
+        var tmp = data.query.results.quote
+        priceN =tmp.LastTradePriceOnly
+        theOne.name =name
+        theOne.priceA=priceA
+        theOne.priceN=priceN
+    })
+    infos.push(theOne)
+    console.log(theOne)
+    res.json(infos)
 })
 app.listen(8000, function () {
     console.log('server running and listennin on localhost:8000')
--- frontend/app/cart_controller.js
+++ frontend/app/cart_controller.js
@@ -1,17 +1,16 @@
 angular.module('currency_search').controller('CartController', ['$scope', '$http', 'Cart', function ($scope, $http, Cart) {
     $http.get('http://localhost:8000/cart').then(function (response) {
         $scope.actions = response.data
-        console.log(response)
+        console.log($scope.actions)
     }, function (error) {
         console.log(error)
     })
-    $scope.sell = function (action) {
-        $http.post('http://localhost:8000/cartSell', JSON.stringify(action)).then(function (response) {
-            window.location.reload()
-            console.log(response)
-        }, function (error) {
+   $scope.getInfo = function (action) {
+        console.log(action)
+        $http.post('http://localhost:8000/infos', JSON.stringify(action)).then(function (response) {
+            $scope.infos = response.data
+        },function(error){
             console.log(error)
         })
     }
-    $scope.getInfo()
 }])
